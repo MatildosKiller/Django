17edd5c377e4ba391180603c5c3e23a39f8ac29c
MatildosKiller <lsnkandrey@outlook.com>
2025-02-09 21:59:49 +0500

    changed views, added templates with complicated context

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,44 +1,59 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template.response import TemplateResponse
 
-# def index(request):
-#     host = request.META["HTTP_HOST"] # получаем адрес сервера
-#     user_agent = request.META["HTTP_USER_AGENT"]    # получаем данные браузера
-#     path = request.path     # получаем запрошенный путь
-     
-#     return HttpResponse(f"""
-#         <p>Host: {host}</p>
-#         <p>Path: {path}</p>
-#         <p>User-agent: {user_agent}</p>
-#     """, headers={"SecretCode": "21234567"}, status=400, reason = "incorrect!!!!")
+from django.http import JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
+
  
+
+class Person:
+  
+    def __init__(self, name, age):
+        self.name = name    # имя человека
+        self.age = age        # возраст человека
+ 
+class PersonEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Person):
+            return {"name": obj.name, "age": obj.age}
+            # # return obj.__dict__
+        return super().default(obj)
+
+
+persons=[Person("Bob",27),Person("Joe",78),Person("Mike",16)]
+
 def index(request):
-    return TemplateResponse(request, "index.html")
+    header = "Данные пользователя"              # обычная переменная
+    langs = ["Python", "Java", "C#"]            # список
+    user ={"name" : "Tom", "age" : 23}          # словарь
+    address = ("Абрикосовая", 23, 45)           # кортеж
+    data = {"header": header, "langs": langs, "user": user, "address": address}
+   
+    return render(request, "index.html", context=data) # context = {"person": Person("Tom")}
 
-def about(request):
-    data = {"header": "Hello Django", "message": "Welcome to Python"}
+# def index(request, id):
     
-    return render(request, "about.html", context=data)
+   
+#     if id in range(0, len(persons)):
+#         return JsonResponse(persons[id], safe=False, encoder=PersonEncoder)
+#     # если нет, то возвращаем ошибку 404
+#     else:
+#         return HttpResponseNotFound("Not Found!!!!!!!!!!!!!")
 
-
-
-def contact(request):
-    return HttpResponse("<h2>Контакты</h2>")
-
-def user(request):
-    age = request.GET.get("age")
-    name = request.GET.get("name")
-    return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
+def access(request, age):
+    # если возраст НЕ входит в диапазон 1-110, посылаем ошибку 400
+    if age not in range(1, 111):
+        return HttpResponseBadRequest("Некорректные данные")
     
-
-
-
-
+    # если возраст больше 17, то доступ разрешен
+    if(age > 17):
+        return HttpResponse("Доступ разрешен")
+    # если нет, то возвращаем ошибку 403
+    else:
+        return HttpResponseForbidden("Доступ заблокирован: недостаточно лет")    
  
-def products(request, id):
-    return HttpResponse(f"Товар {id}")
-
+def products(request):
+    return HttpResponse("Список товаров")
  
 def new(request):
     return HttpResponse("Новые товары")
@@ -46,8 +61,25 @@ def new(request):
 def top(request):
     return HttpResponse("Наиболее популярные товары")
 
-def comments(request, id):
-    return HttpResponse(f"Комментарии о товаре {id}")
- 
-def questions(request, id):
-    return HttpResponse(f"Вопросы о товаре {id}")
+def user(request):
+    age = request.GET.get("age", 0)
+    name = request.GET.get("name", "Undefined")
+    return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
+
+
+# установка куки
+def set(request):
+    # получаем из строки запроса имя пользователя
+    username = request.GET.get("username", "Undefined")
+    # создаем объект ответа
+    response = HttpResponse(f"Hello {username}")
+    # передаем его в куки
+    response.set_cookie("username", username)
+    response.set_cookie("dog", "Bobby")
+    return response
+
+# получение куки
+def get(request,key):
+    # получаем куки с ключом username
+    value = request.COOKIES[key]
+    return HttpResponse(f"Hello {value}")
