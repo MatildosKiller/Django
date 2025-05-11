4b259e81fe7395343a10400f629b1aeda10ded08
MatildosKiller <lsnkandrey@outlook.com>
2025-05-11 17:08:14 +0500

    управление стилями формы

--- metanit/hello/forms.py
+++ metanit/hello/forms.py
@@ -1,8 +1,8 @@
 from django import forms
  
 class UserForm(forms.Form):
-    name = forms.CharField(label="Ваше имя",min_length=2, max_length=20)
-    age = forms.IntegerField(help_text="Введите свой возраст", label="Ваш возраст",min_value=1, max_value=100)
+    name = forms.CharField(label="Ваше имя",min_length=2, max_length=20, widget=forms.TextInput(attrs={"class":"myfield"}))
+    age = forms.IntegerField(help_text="Введите свой возраст", label="Ваш возраст",min_value=1, max_value=100,widget=forms.NumberInput(attrs={"class":"myfield"}))
     email = forms.EmailField(label="Ваша почта", help_text="в поле обязательно должен присутствовать символ @")
     about = forms.CharField(label="О себе", help_text="пару слов", required=False)
     required_css_class = "field"
