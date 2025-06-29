90df8d3c39da382bfb832802095c09d0c18bb554
MatildosKiller <lsnkandrey@outlook.com>
2025-06-29 17:02:16 +0500

    обновляли объекты через модель

--- metanit/hello/models.py
+++ metanit/hello/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import F
 import asyncio
 # Create your models here.
 
@@ -8,6 +9,35 @@ class Person(models.Model):
     age = models.IntegerField()
 
 
-async def acreate_person():
-    person = await Person.objects.acreate(name="Tim", age=26)
+async def acreate_person(name_arg, age_arg):
+    person = await Person.objects.acreate(name=name_arg, age=age_arg)
     print(person.name)
+
+async def abulc_create_persons():
+    people = Person.objects.bulk_create([Person(name="Kate", age=24),Person(name="Ann", age=21),])
+    
+
+
+async def update_or_create_person():
+    values_for_update={"name":"Bob", "age": 31}
+    bob, created = Person.objects.aupdate_or_create(id=2, defaults = values_for_update)
+    if(created):print("создан новый объект")
+
+async def update_person(_id):
+    Person.objects.filter(id=_id).aupdate(age = F("age") + 1) 
+
+
+async def bulk_update_person():
+    first_person = Person.objects.get(id=1)
+    first_person.name = "Tomas"
+    second_person = Person.objects.get(id=2)
+    second_person.age = 29
+    number = Person.objects.abulk_update([first_person, second_person], ["name", "age"])
+    print(number)
+
+async def delete_person(_id):
+   pers = Person.objects.get(id=_id)
+   pers.adelete() 
+
+async def delete_person_querry(_id):
+   Person.objects.filter(id=_id).delete()
