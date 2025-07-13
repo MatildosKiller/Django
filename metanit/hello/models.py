a65f6fb8217659db17c9758ef2d98cc5a05cb169
Andrew <lsnkandrey@yandex.ru>
2025-07-13 19:29:18 +0500

    поработали с фильтрами

--- metanit/hello/models.py
+++ metanit/hello/models.py
@@ -1,43 +1,43 @@
 from django.db import models
-from django.db.models import F
 import asyncio
 # Create your models here.
 
- 
+
 class Person(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField()
 
+people = Person.objects.bulk_create([
+    Person(name="Kate", age=24),
+    Person(name="Ann", age=21),
+])
 
-async def acreate_person(name_arg, age_arg):
-    person = await Person.objects.acreate(name=name_arg, age=age_arg)
+people_by_age1 = Person.objects.filter(age=32)
+for person in people_by_age1:
+    print(f"{person.name} - {person.age}")
+
+#     # получаем пользователей, у которых имя равно NULL
+# people_by_name = Person.objects.filter(name__exact=None)
+
+#получаем всех пользователей с заданным параметром, игнорируя регистр символов
+# tom = Person.objects.get(name__iexact="tom")
+
+people1 = Person.objects.filter(name__contains="o")
+
+people3 = Person.objects.filter(age__in=[32, 35, 38])
+# получаем пользователей, у которых возраст меньше или равен 32 
+people7 = Person.objects.filter(age__lte=32)
+# получаем пользователей, у которых имя начинается с To или to
+people8 = Person.objects.filter(name__istartswith="To")
+
+
+ 
+# получаем пользователей, у которых возраст установлен
+people78 = Person.objects.filter(age__isnull=False)
+
+# получаем пользователей, у которых имя заканчивается на am или om
+people9 = Person.objects.filter(name__regex=r"(am|om)$")
+
+async def acreate_person():
+    person = await Person.objects.acreate(name="Tim", age=26)
     print(person.name)
-
-async def abulc_create_persons():
-    people = Person.objects.bulk_create([Person(name="Kate", age=24),Person(name="Ann", age=21),])
-    
-
-
-async def update_or_create_person():
-    values_for_update={"name":"Bob", "age": 31}
-    bob, created = Person.objects.aupdate_or_create(id=2, defaults = values_for_update)
-    if(created):print("создан новый объект")
-
-async def update_person(_id):
-    Person.objects.filter(id=_id).aupdate(age = F("age") + 1) 
-
-
-async def bulk_update_person():
-    first_person = Person.objects.get(id=1)
-    first_person.name = "Tomas"
-    second_person = Person.objects.get(id=2)
-    second_person.age = 29
-    number = Person.objects.abulk_update([first_person, second_person], ["name", "age"])
-    print(number)
-
-async def delete_person(_id):
-   pers = Person.objects.get(id=_id)
-   pers.adelete() 
-
-async def delete_person_querry(_id):
-   Person.objects.filter(id=_id).delete()
